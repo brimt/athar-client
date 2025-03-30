6055815f2c6a35a723ec41e4403353e2d7191e25
MoBadraan <mohamedbadran1492008@gmail.com>
2025-03-30 21:36:05 +0200

    Suii QR Code UI

--- dashboard/assets/js/qr.js
+++ dashboard/assets/js/qr.js
@@ -1,25 +1,12 @@
-function onScanSuccess(decodedText, decodedResult) {
-  fetch('/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      qrCode: decodedText
-    })
-  }).then(() => {
-    window.location.href = '../';
-  });
-}
-
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
-
-let html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader",
-  { fps: 10, qrbox: { width: 250, height: 250 } },
-  /* verbose= */ false);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+document.addEventListener('DOMContentLoaded', () => {
+  const html5QrCode = new Html5Qrcode('reader');
+  const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    document.getElementById('success').style.display = 'block';
+    document.getElementById('reader').style.display = 'none';
+    setTimeout(() => {
+      location.href = '/';
+    }, 2000);
+  };
+  const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+  html5QrCode.start({ facingMode: 'environment' }, config, qrCodeSuccessCallback);
+});
